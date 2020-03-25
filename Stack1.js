65283d9745dfff2ea3ea96f67f7a51506dfecb1e
ogbeche77 <ogbeche77@yahoo.com>
2020-03-25 11:22:11 +0100

    excercise

--- Stack1.js
+++ Stack1.js
@@ -1,4 +1,4 @@
-class Stack extends Array {
+/*class Stack extends Array {
     constructor(...elems){ 
         super(...elems)  //super represent array
     }
@@ -12,12 +12,29 @@ class Stack extends Array {
         if(this.length === 0) throw new Error ("stack is damn empty")
         return this[this.length - 1]
     }
+    size(){
+        return this.length
+    }
 
 }
-
-
 
 const s = new Stack(1,2,3)
 s.push(100)
 
-console.log(s.peek())
+console.log(s.peek()) */
+
+/* Check of the following strings are correctly allign */
+
+class Stack extends Array{
+
+}
+const string1 ="[[{([[({})]])}]]"
+const string2 ="[[{([[({})]])}]"
+
+function check(string) {
+    const arr = string.split('') // returns array of individual string
+
+
+
+
+}

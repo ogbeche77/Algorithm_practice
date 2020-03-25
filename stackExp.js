1165fd05d072280831403f1c0991ae80255fcd3f
ogbeche77 <ogbeche77@yahoo.com>
2020-03-25 09:54:26 +0100

    example

--- stackExp.js
+++ stackExp.js
@@ -24,8 +24,16 @@ this.size = function() {
 }
 
 //Returns the value at the end of stack
-this.peek = function(value) {
+this.peek = function() {
     return this.storage[this.count-1];
 }
 
 }
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());

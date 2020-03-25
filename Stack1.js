cca44877fa4d2bb67b86ce694faba93bab2cc6db
ogbeche77 <ogbeche77@yahoo.com>
2020-03-25 11:04:57 +0100

    peek

--- Stack1.js
+++ Stack1.js
@@ -6,9 +6,13 @@ class Stack extends Array {
     pop(){
         if(this.length === 0) throw new Error("Nothng here to pop mehn")
         super.pop()
+    } 
+
+    peek() {
+        if(this.length === 0) throw new Error ("stack is damn empty")
+        return this[this.length - 1]
     }
 
-    
 }
 
 
@@ -16,4 +20,4 @@ class Stack extends Array {
 const s = new Stack(1,2,3)
 s.push(100)
 
-console.log(s)
+console.log(s.peek())

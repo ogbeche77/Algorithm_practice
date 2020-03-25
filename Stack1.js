be6c131d4adac7e507b885ec1c9f27391a7658a7
ogbeche77 <ogbeche77@yahoo.com>
2020-03-25 10:59:01 +0100

    pop

--- Stack1.js
+++ Stack1.js
@@ -1,5 +1,19 @@
 class Stack extends Array {
-    constructor(...elems){  //super represent array
-
+    constructor(...elems){ 
+        super(...elems)  //super represent array
     }
+
+    pop(){
+        if(this.length === 0) throw new Error("Nothng here to pop mehn")
+        super.pop()
+    }
+
+    
 }
+
+
+
+const s = new Stack(1,2,3)
+s.push(100)
+
+console.log(s)
